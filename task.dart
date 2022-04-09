afaeebe37dd4c8f6ec99fbe661a2050ff094c59b
Archit Sharma <90101251+Architsharma7@users.noreply.github.com>
2022-04-09 18:32:41 +0530

    Add files via upload

---
+++ task.dart
@@ -0,0 +1,25 @@
+class Task {
+  String task;
+  DateTime time;
+
+  Task({required this.task, required this.time});
+
+  factory Task.fromString(String task) {
+    return Task(
+      task: task,
+      time: DateTime.now(),
+    );
+  }
+  factory Task.fromMap(Map<String, dynamic>map) {
+    return Task(
+      task: map['task'],
+      time: DateTime.fromMicrosecondsSinceEpoch(map['time']),
+    );
+  }
+  Map<String, dynamic> getMap() {
+    return {
+      'task': task,
+      'time': time.millisecondsSinceEpoch,
+    };
+  }
+}
